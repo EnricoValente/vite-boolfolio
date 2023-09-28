dc4163b70d6382ad26f3c2f53845a2f24f4be5f5
EnricoValente <enricovalente42@gmail.com>
2023-09-28 18:26:25 +0200

    edit risultati in pagina

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -3,13 +3,15 @@
 	export default {
 		data() {
 			return {
-				title:'Main'
+				
+                projects: []
 			}
 		},
         created() {
             axios.get('http://localhost:8000/api/projects')
                  .then(response=> {
                     console.log(response)
+                    this.projects= response.data.projects.data;
                  });
         }
 		
@@ -18,15 +20,43 @@
 
 
 <template>
-    <div>
-        <h2>
-            {{ title }}
-        </h2>
-    </div>
+    <main>
+        <section>
+            <div class="projects-container">
+                <div v-for="project in projects" :key="project.id">
+                    {{ project.title }}
+
+                    <div>
+                        {{ project.description }}
+                    </div>
+
+                    <div>
+                        {{ project.chief }}
+                    </div>
+
+                    <div>
+                        {{ project.start_date }}
+                    </div>
+
+                    <div>
+                        {{ project.end_date }}
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 </template>
 
 <style scoped>
-    h2 {
-        color: greenyellow;
-    }
+   .projects-container {
+    display: flex;
+    flex-wrap: wrap;
+   }
+
+   .projects-container > * {
+    width: calc(100% / 4);
+    border: 1px solid blue;
+    padding: 10px;
+    text-align: center;
+   }
 </style>
