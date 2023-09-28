9fa5538e8915fb998f11449f8160d8ce74ec1308
EnricoValente <enricovalente42@gmail.com>
2023-09-28 16:30:01 +0200

    init progetto

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,25 @@
+<script>
+	export default {
+		data() {
+			return {
+				title:'Footer'
+			}
+		}
+		
+	}
+</script>
+
+
+<template>
+    <div>
+        <h2>
+            {{ title }}
+        </h2>
+    </div>
+</template>
+
+<style scoped>
+    h2 {
+        color: darkcyan;
+    }
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,25 @@
+<script>
+	export default {
+		data() {
+			return {
+				title:'Header'
+			}
+		}
+		
+	}
+</script>
+
+
+<template>
+    <div>
+        <h2>
+            {{ title }}
+        </h2>
+    </div>
+</template>
+
+<style scoped>
+    h2 {
+        color: blueviolet;
+    }
+</style>
