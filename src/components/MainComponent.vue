98bca254ec65559d80630a9cc03beb5501841cf4
EnricoValente <enricovalente42@gmail.com>
2023-09-28 17:40:50 +0200

    edit richiesta axios

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,10 +1,17 @@
 <script>
+    import axios from 'axios';
 	export default {
 		data() {
 			return {
 				title:'Main'
 			}
-		}
+		},
+        created() {
+            axios.get('http://localhost:8000/api/projects')
+                 .then(response=> {
+                    console.log(response)
+                 });
+        }
 		
 	}
 </script>
